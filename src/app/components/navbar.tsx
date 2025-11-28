cd2fe7619d80b9635087801b396138b2fb19f447
IvanIvPe <pesicivan2002@gmail.com>
2025-11-28 15:32:27 +0100

    Updates

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
+import Link from 'next/link';
+import styles from './navbar.module.css';
+import Image from 'next/image';
 
 
 const Navbar: React.FC = () => {
   return (
-    <nav>
-      <a href="/">
-        List App
-      </a>
-      
-      <ul>
+    <nav className={styles.navbar}>
+      <Link href="/" className={styles.logo}>
+        <Image src="/listlogo.png" alt="Logo" width={32} height={32} />
+        <span className={styles.logoText}>List App</span>
+      </Link>
+
+      <ul className={styles.navList}>
         <li>
-          <a href="/">Home</a>
-        </li>
-                <li>
-          <a href="/list">List</a>
+          <Link href="/" className={styles.navLink}>Home</Link>
         </li>
         <li>
-          <a href="/about">About</a>
+          <Link href="/list" className={styles.navLink}>List</Link>
         </li>
         <li>
-          <a href="/contact">Contact</a>
+          <Link href="/about" className={styles.navLink}>About</Link>
+        </li>
+        <li>
+          <Link href="/contact" className={styles.navLink}>Contact</Link>
         </li>
       </ul>
     </nav>
